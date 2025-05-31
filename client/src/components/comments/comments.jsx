4bde3a9bcef868ec05ee183528326b26541658ff
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-05-31 21:42:35 +0700

    add emoji picker

--- client/src/components/comments/comments.jsx
+++ client/src/components/comments/comments.jsx
@@ -1,6 +1,92 @@
+import { useState } from 'react';
 import './comments.css'
+import EmojiPicker from 'emoji-picker-react';
 export default function Comments() {
+  const [open, setOpen] = useState(false)
   return (
-    <div className='comments'>comments</div>
+    <div className='comments'>
+      <div className="commentList">
+        <span className='commentCount'>5 comments</span>
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+
+
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+
+        <div className="comment">
+          <img src="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className='commentUsername'>John Dang</span>
+            <p className='commentText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+            <span className='commentTime'>1h</span>
+          </div>
+        </div>
+      </div>
+      <form action="" className='commentForm'>
+        <input type="text" placeholder='Add comment'   />
+        <div className="emoji">
+          <div onClick={()=> setOpen(prev =>!prev)}>🐷</div>
+          {open && <div className='emojiPicker'>
+            <EmojiPicker/>
+          </div>}
+        </div>
+      </form>
+    </div>
   )
 }
