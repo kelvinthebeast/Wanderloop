d0866bdf18ac69ac8cef3c5246aaf7eb031e55a4
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-05-31 16:01:02 +0700

    ver 1 add online glocal img

--- client/src/components/galleryItem/galleryItem.jsx
+++ client/src/components/galleryItem/galleryItem.jsx
@@ -1,10 +1,18 @@
 
 import './galleryItem.css'
 import { Link } from 'react-router'
+import { Image } from '@imagekit/react';
 export default function GalleryItem({item}) {
   return (
     <div className='galleryItem' style={{gridRowEnd:`span ${Math.ceil(item.height/100)}`}}>
-      <img src={item.media} alt="" />
+      {/* <img src={item.media} alt="" /> */}
+       <Image
+        urlEndpoint={import.meta.env.VITE_URL_IMAGE_ENDPOINT}
+        src={item.media}
+        // width={item.width}
+        // height={item.height}
+        alt="Picture of the author"
+    />
       <Link to={`/pin/${item.id}`} className='overlay' ></Link>
       <button className="saveButton">Save</button>
       <div className="overlayIcons">
