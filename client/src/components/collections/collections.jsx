f60f6d15a7a55e86582a9f9ad6537393a35b32bc
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-05-31 23:08:12 +0700

    fe ses 3

---
+++ client/src/components/collections/collections.jsx
@@ -0,0 +1,109 @@
+import './collections.css'
+
+
+const Collections = () => {
+  return (
+    <div className='collections'>
+      {/* COLLECTION */}
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+
+      <div className="collection">
+        <img src="/pins/pin1.jpeg" alt=""/>
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Collections
--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,7 +1,34 @@
 
+import { useState } from 'react';
 import './profilePage.css'
-export default function ProfilePage() {
+import Collections from '../../components/collections/collections';
+import Gallery from '../../components/Gallery/Gallery'
+
+const ProfilePage = () => {
+  const [type, setType] = useState("saved")
   return (
-    <div>profilePage</div>
-  )
-}
+    <div className="profilePage">
+      <img className='profileImg' src="/general/noAvatar.png" alt="" width={100} height={100} />
+      <h1 className="profileName">John Doe</h1>
+      <span className="profileUsername">@johndoe</span>
+      <div className="followCounts">10 followers · 20 followings</div>
+      <div className="profileInteractions">
+        <img src="/general/share.svg" alt="" />
+        <div className="profileButtons">
+          <button>Message</button>
+          <button>Follow</button>
+        </div>
+        <img src="/general/more.svg" alt="" />
+
+      </div>
+      <div className="profileOptions">
+        <span onClick={()=> setType("created")} className={type==="created" ? "active" : ""}>Created</span>
+        <span onClick={()=> setType("saved")} className={type==="saved" ? "active" : ""}>Saved</span>
+
+      </div>
+      {type === "created" ? <Gallery/> : <Collections/>}
+    </div>
+  );
+};
+
+export default ProfilePage;
