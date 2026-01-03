e0456cfda793a018191b75647db60bcef76c2d27
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2026-01-03 21:01:26 +0700

    add

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -1,7 +1,7 @@
 import Pin from '../models/pin.model.js'
 
 export const getPins = async (req, res) => {
-  const pins = await Pin.find({})
+  const pins = await Pin.find({}).limit(21).skip(21)
   return res.status(200).json({
     pins: pins
   })
